c553952228e3a03c7be702127667977dd34463bf
Brian Bechaud <brian_bechaud@yahoo.com>
2023-06-13 10:39:57 -0400

    Added missing tittle and description

--- js/script9.js
+++ js/script9.js
@@ -1,3 +1,13 @@
+// Codewars challenge 9 (Level 7 kyu): Sort array by string length
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"] => ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
 function sortByLength (array) {
     return array.sort((a, b) => a.length - b.length);
   };
