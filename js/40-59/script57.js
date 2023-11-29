dfb53144ba115d171b618b1abbca754ea3a9e195
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2023-11-29 14:12:52 -0500

    Day 57 of CW commits

---
+++ js/40-59/script57.js
@@ -0,0 +1,19 @@
+// Codewars Challenge 56 (Level 7kyu): Find the anonymous function
+
+// Find the anonymous function in the given array and use the function to filter the array
+
+// Input: Your input. First Parameter will be an array with an anonymous function somewhere in the lot, The second Parameter will be an array which you will filter using the anonymous function you find.
+
+// Output: Your output. Output a filtered version of the second parameter using the function found in the first parameter.
+
+const FindFunction = function(func, arr) {
+    let useFunc = func.find((e) => typeof e === 'function')
+    let result = arr.filter(useFunc)
+    
+    return result
+  }
+
+
+// Or in one line:
+
+const FindFunction2 = (func, arr) => arr.filter(func.find(e => typeof e === 'function'))
