786649d52592839b12e103da2de15458bbd60f1e
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2023-12-06 10:20:58 -0500

    Day 63 of CW commits

---
+++ js/60-79/script63.js
@@ -0,0 +1,51 @@
+// Codewars Challenge 63 (Level 6kyu): What's A Name In?
+
+// Write a function, taking two strings in parameter, that tests whether or not the first string contains all of the letters of the second string, in order.
+
+// The function should return true if that is the case, and else false. Letter comparison should be case-INsensitive.
+
+// Examples
+//     "Across the rivers", "chris" --> true
+//       ^      ^  ^^   ^
+//       c      h  ri   s
+                
+//     Contains all of the letters in "chris", in order.
+// ----------------------------------------------------------
+//     "Next to a lake", "chris" --> false
+
+//     Contains none of the letters in "chris".
+// --------------------------------------------------------------------
+//     "Under a sea", "chris" --> false
+//          ^   ^
+//          r   s
+
+//     Contains only some of the letters in "chris".
+// --------------------------------------------------------------------
+//     "A crew that boards the ship", "chris" --> false
+//        cr    h              s i
+//        cr                h  s i  
+//        c     h      r       s i
+//                  ...
+          
+//     Contains all of the letters in "chris", but not in order.
+// --------------------------------------------------------------------
+//     "A live son", "Allison" --> false
+//      ^ ^^   ^^^
+//      A li   son
+            
+//     Contains all of the correct letters in "Allison", in order, but not enough of all of them (missing an 'l').
+
+function nameInStr(str, name){
+    name = name.toLowerCase()
+    str = str.toLowerCase()
+    
+    let index = 0
+    
+    for (let char of str) {
+      if (char === name[index])
+        index++      
+    }
+
+    return index === name.length
+    
+  }
