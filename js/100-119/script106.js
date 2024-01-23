cbd3fc3a3959de8ef435ccc6d696e82be5b066e7
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2024-01-23 17:58:57 -0500

    Day 106 of CW/Leetcode commits

---
+++ js/100-119/script106.js
@@ -0,0 +1,20 @@
+// Day 106 LeetCode 1071.(easy) Greatest Common Divisor of Strings
+
+// For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+// Example:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1) {
+     return "";
+ }
+
+ const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+ 
+ return str1.substring(0, gcd(str1.length, str2.length));
+};
